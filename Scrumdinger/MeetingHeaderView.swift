a15fce5ffcc46d4dc9ae82492b9ad47401e88fc6
Hailey Fox <hailey.p.fox.22@dartmouth.edu>
2021-09-22 12:05:42 -0400

    Updated files

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -10,9 +10,22 @@ import SwiftUI
 struct MeetingHeaderView: View {
     let secondsElapsed: Int
     let secondsRemaining: Int
+    private var progress: Double {
+        guard secondsRemaining > 0 else { return 1 }
+        let totalSeconds = Double(secondsElapsed + secondsRemaining)
+        return Double(secondsElapsed) / totalSeconds
+    }
+    private var minutesRemaining: Int {
+        secondsRemaining / 60
+    }
+    private var minutesRemainingMetric: String {
+        minutesRemaining == 1 ? "minute" : "minutes"
+    }
+    let scrumColor: Color
     var body: some View {
         VStack {
-            ProgressView(value: 5, total: 15)
+//            ProgressView(value: progress)
+//                .progressViewStyle(ScrumProgressViewStyle(scrumColor: scrumColor))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -23,19 +36,23 @@ struct MeetingHeaderView: View {
                 VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                         .font(.caption)
-                    Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                    HStack {
+                        Text("\(secondsRemaining)")
+                        Image(systemName: "hourglass.tophalf.fill")
+                    }
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel(Text("Time remaining"))
-        .accessibilityValue(Text("10 minutes"))
+        .accessibilityValue(Text("\(minutesRemaining) \(minutesRemainingMetric)"))
+        .padding([.top, .horizontal])
     }
 }
 
 struct MeetingHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingHeaderView()
+        MeetingHeaderView(secondsElapsed: 60, secondsRemaining: 180, scrumColor: DailyScrum.data[0].color)
             .previewLayout(.sizeThatFits)
     }
 }
